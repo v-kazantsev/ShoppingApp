c46734d0398fe4cd49075f233f8ebdc6692f49ab
v-kazantsev <vvs201@mail.ru>
2022-12-19 13:39:57 +0700

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by mac on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
